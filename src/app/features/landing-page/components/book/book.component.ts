016abd406fa10a6cc0ff2fce190c5fdc649ae43d
Reinhard Odhiambo <reinhard.odhiambo@medbookafrica.com>
2024-05-02 17:06:56 +0300

    initiated landing page feature

---
+++ src/app/features/landing-page/components/book/book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.sass'
+})
+export class BookComponent {
+
+}
