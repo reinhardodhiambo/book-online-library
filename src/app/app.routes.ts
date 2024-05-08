b944c5b8b52dbd740d7e2d56a5497948159bcfea
Reinhard Odhiambo <reinhard.odhiambo@medbookafrica.com>
2024-05-08 12:46:37 +0300

    added wishlist component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from "@features/landing-page/components/home/home.component";
 import {BookDetailComponent} from "@shared/components/books/book/book-detail/book-detail.component";
+import {WishlistComponent} from "@features/wishlist-page/components/wishlist/wishlist.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'book-details/works/:id', component: BookDetailComponent},
+  {path: 'wishlist', component: WishlistComponent},
 ];
--- src/app/shared/components/books/book/book-detail/book-detail.component.ts
+++ src/app/shared/components/books/book/book-detail/book-detail.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute} from "@angular/router";
 import {HomeService} from "@features/landing-page/services/home/home.service";
 import {SubjectSearchResponse} from "@shared/models/subject-search-response";
 import {ToasterService} from "@core/services/toaster/toaster.service";
+import {WishlistService} from "@features/wishlist-page/services/wishlist/wishlist.service";
 
 @Component({
   selector: 'app-book-detail',
@@ -18,7 +19,8 @@ export class BookDetailComponent implements OnChanges, OnInit {
   private activatedRoute = inject(ActivatedRoute);
   workId = this.activatedRoute.snapshot.params['id'];
 
-  constructor(private homeService: HomeService, private toasterService:ToasterService) {
+  constructor(private homeService: HomeService, private toasterService: ToasterService,
+              private wishlistService: WishlistService) {
   }
 
 
@@ -82,31 +84,35 @@ export class BookDetailComponent implements OnChanges, OnInit {
       }
     )
   }
+
   addToWishlist() {
-    const payload = {
-      add: [
-        {key: this.book.key},
-      ]
-    }
-    const sub = this.homeService.addBookToWishList(payload).subscribe(
-      {
-        next: (res: any) => {
+    if (confirm("Add item to wishlist?")) {
 
-          this.toasterService.show('success', 'Item added', 'Item was added to wishlist successfully.');
 
-        },
-        error: (err) => {
-          this.toasterService.show('error', 'Error', err.message);
-
-        },
-        complete: () => {
-          sub.unsubscribe();
-
-        },
+      const payload = {
+        "add": [
+          {key: this.book.key},
+        ]
       }
-    )
+      const sub = this.wishlistService.updateSeedList(payload).subscribe(
+        {
+          next: (res: any) => {
 
+            this.toasterService.show('success', 'Item added', 'Item was added to wishlist-page successfully.');
 
+          },
+          error: (err) => {
+            this.toasterService.show('error', 'Error', err.message);
+
+          },
+          complete: () => {
+            sub.unsubscribe();
+
+          },
+        }
+      )
+
+    }
 
   }
 
